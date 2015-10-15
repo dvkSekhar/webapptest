df4ac9031eee92718a3c50314bfe2482511fcefd
dvkSekhar <dvksekhar@gmail.com>
2015-10-14 21:37:57 -0400

    Update DatabaseUtil.java

--- src/com/proquest/interview/util/DatabaseUtil.java
+++ src/com/proquest/interview/util/DatabaseUtil.java
@@ -10,20 +10,41 @@ import java.sql.Statement;
  * @author rconklin
  */
 public class DatabaseUtil {
+	private static final String INSERT_CYN = "INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Dave Williams','(231) 502-1236', '285 Huron St, Port Austin, MI')";
+	private static final String INSERT_JOHN = "INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Chris Johnson','(321) 231-7876', '452 Freeman Drive, Algonac, MI')";
+	private static final String CREATE_TABLE = "CREATE TABLE PHONEBOOK (NAME varchar(255), PHONENUMBER varchar(255), ADDRESS varchar(255))";
 	public static void initDB() {
+		Connection cn = null;
 		try {
-			Connection cn = getConnection();
+			cn = getConnection();
 			Statement stmt = cn.createStatement();
-			stmt.execute("CREATE TABLE PHONEBOOK (NAME varchar(255), PHONENUMBER varchar(255), ADDRESS varchar(255))");
-			stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Chris Johnson','(321) 231-7876', '452 Freeman Drive, Algonac, MI')");
-			stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Dave Williams','(231) 502-1236', '285 Huron St, Port Austin, MI')");
-			cn.commit();
-			cn.close();
+			insertDB(stmt, CREATE_TABLE);
+			insertDB(stmt, INSERT_JOHN);
+			insertDB(stmt, INSERT_CYN);
 		} catch (Exception ex) {
 			ex.printStackTrace();
+		}finally{
+			try {
+				cn.commit();
+				cn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
 	}
+	/**
+	 * @param stmt
+	 * @throws SQLException
+	 */
+	public static void insertDB(Statement stmt, String sql)  {
+		try {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			if(e.getMessage().indexOf("object name already exists:") <-1)
+				e.printStackTrace();
+		}
+	}
+	
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("org.hsqldb.jdbcDriver");
 		return DriverManager.getConnection("jdbc:hsqldb:mem", "sa", "");
